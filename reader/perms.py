4f4da1f80d7cc760b06dd071a91f0897a5151668
Travis Vick <Travis Vick>
2016-08-01 11:45:54 -0700

    start over

--- reader/perms.py
+++
@@ -1,7 +0,0 @@
-from permission.logics import AuthorPermissionLogic
-from permission.logics import CollaboratorsPermissionLogic
-
-PERMISSION_LOGICS = (
-    ('your_app.Article', AuthorPermissionLogic()),
-    ('your_app.Article', CollaboratorsPermissionLogic()),
-)
